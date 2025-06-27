36b8815ea70fdd2c956f783b555deb97b73e3dc4
Níz Peña <nizadiel5@gmail.com>
2025-06-26 23:35:28 -0700

    Script de movimiento basico corregido

--- scripts/character/red.gd
+++ scripts/character/red.gd
@@ -1,27 +1,23 @@
 extends CharacterBody2D
 
-var speed := 300
-var dash_speed := 600
-var is_dashing := false
+var speed = 150
+var run_speed = 2
+var run   = false
 
-func _physics_process(delta: float) -> void:
-	handle_movement()
+func _physics_process(delta):
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	if run == false:
+		velocity = direction * speed
+	else:
+		velocity = (direction * speed) * run_speed
+	
+	
+	move_and_slide()
 	handle_dash()
 
-func handle_movement() -> void:
-	var direction := Vector2.ZERO
-	if Input.is_action_pressed("ui_d"):
-		direction.x += 1
-	if Input.is_action_pressed("ui_a"):
-		direction.x -= 1
-	if Input.is_action_pressed("ui_s"):
-		direction.y += 1
-	if Input.is_action_pressed("ui_w"):
-		direction.y -= 1
-	velocity = direction.normalized()
-
 func handle_dash() -> void:
-	if Input.is_action_just_pressed("ui_shift"):
-		is_dashing = true
-	elif Input.is_action_just_released("ui_shift"):
-		is_dashing = false
+	if Input.is_action_just_pressed("ui_x"):
+		run = true
+	elif Input.is_action_just_released("ui_x"):
+		run = false
