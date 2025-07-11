aa3f61829bf020ea199165aaded4e407cf73e662
Níz Peña <nizadiel5@gmail.com>
2025-07-11 15:32:05 -0700

    Movimiento del personaje programado

--- scripts/character/red.gd
+++ scripts/character/red.gd
@@ -1,23 +1,20 @@
 extends CharacterBody2D
 
-var speed = 150
-var run_speed = 2
-var run   = false
+var speed          = 150
+var run_multiplier = 2
+var run = false
 
-func _physics_process(delta):
+func _ready() -> void:
+	add_to_group("Player")
+
+func _physics_process(_delta):
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	
-	if run == false:
-		velocity = direction * speed
-	else:
-		velocity = (direction * speed) * run_speed
-	
-	
+	velocity = direction * speed * (run_multiplier if run else 1.0)
 	move_and_slide()
 	handle_dash()
 
 func handle_dash() -> void:
 	if Input.is_action_just_pressed("ui_x"):
 		run = true
-	elif Input.is_action_just_released("ui_x"):
+	elif Input.is_action_just_released("ui_z"):
 		run = false
